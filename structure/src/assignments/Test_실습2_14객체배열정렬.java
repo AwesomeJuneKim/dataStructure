6e2d54be4b803e111e27e3dd95698c42e523a9a4
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-06 17:53:38 +0900

    과제 완료

--- structure/src/assignments/Test_실습2_14객체배열정렬.java
+++ structure/src/assignments/Test_실습2_14객체배열정렬.java
@@ -3,50 +3,77 @@ package assignments;
 import java.util.Arrays;
 
 //5번 실습 - 2장 실습 2-14를 수정하여 객체 배열의 정렬 구현
-class PhyscData implements Comparable<PhyscData>{
+class PhyscData implements Comparable<PhyscData> {
 	String name;
 	int height;
 	double vision;
-
+public PhyscData(String name, int height, double vision) {
+	this.name=name;
+	this.height=height;
+	this.vision=vision;
+}
 	@Override
-	public String toString() {//toString은 모든 자바 객체에 포함되므로 오버라이딩 가능하다.
-
+	public String toString() {// toString은 모든 자바 객체에 포함되므로 오버라이딩 가능하다.
+//객체출력 
+		return "[" + name + ", " + height + "," + vision + "]";
 	}
+
 	@Override
 	public int compareTo(PhyscData p) {
-
+		if (this.name.compareTo(p.name) > 0)
+			return 1;
+		else if (this.name.compareTo(p.name) < 0)
+			return -1;
+		else
+			return 0;
 	}
+
 	public int equals(PhyscData p) {
+		if (this.name.compareTo(p.name) > 0)
+			return 1;
+		else if (this.name.compareTo(p.name) < 0)
+			return -1;
+		else
+			return 0;
 
 	}
 }
+
 public class Test_실습2_14객체배열정렬 {
-	static void swap(PhyscData[]arr, int ind1, int ind2) {
-		
+	static void swap(PhyscData[] arr, int ind1, int ind2) {
+		PhyscData t = arr[ind1];
+		arr[ind1] = arr[ind2];
+		arr[ind2] = t;
+
 	}
-	static void sortData(PhyscData []arr) {
-		for(int i=0; )
-			for(int j=0; )
-				if(arr[i]>arr[j])
-					swap(arr,i,j);
-		
+
+	static void sortData(PhyscData[] arr) {
+		for (int i = 0; i < arr.length; i++)
+			for (int j = i + 1; j < arr.length; j++)
+				if (arr[i].compareTo(arr[j]) > 0)
+					swap(arr, i, j);
+
 	}
+
 	public static void main(String[] args) {
-		PhyscData[] data = {
-				new PhyscData("홍길동", 162, 0.3),
+		PhyscData[] data = { 
+				new PhyscData("홍길동", 162, 0.3), 
 				new PhyscData("홍동", 164, 1.3),
-				new PhyscData("홍길", 152, 0.7),
-				new PhyscData("김홍길동", 172, 0.3),
+				new PhyscData("홍길", 152, 0.7), 
+				new PhyscData("김홍길동", 172, 0.3), 
 				new PhyscData("이길동", 182, 0.6),
-				new PhyscData("박길동", 167, 0.2),
-				new PhyscData("최길동", 169, 0.5),
-		};
+				new PhyscData("박길동", 167, 0.2), 
+				new PhyscData("최길동", 169, 0.5), };
 		showData(data);
 		sortData(data);
-		//Arrays.sort(null, null);//comparator가 필요하다 
+		// Arrays.sort(null, null);//comparator가 필요하다
 		showData(data);
 	}
-	static void showData(PhyscData[]arr) {
+
+	static void showData(PhyscData[] arr) {
+		for (PhyscData i : arr) {
+			System.out.println(i + " ");
+		}
 
 	}
 
--- structure/src/assignments/Test_실습2_14스트링배열정렬.java
+++ structure/src/assignments/Test_실습2_14스트링배열정렬.java
@@ -1,7 +1,7 @@
 package assignments;
 
 public class Test_실습2_14스트링배열정렬 {
-public class 실습2_14스트링배열정렬 {
+
 	public static void main(String[] args) {
 		String []data = {"apple","grape","persimmon", "pear","blueberry", "strawberry", "melon", "oriental melon"};
 
@@ -10,20 +10,24 @@ public class 실습2_14스트링배열정렬 {
 		showData(data);
 	}
 	static void showData(String[]arr) {
+		for(int i=0; i<arr.length;i++)
+			System.out.print(arr[i]+" ");
+		System.out.println();
 
 	}
 
 	static void swap(String[]arr, int ind1, int ind2) {
-		
+		String t= arr[ind1];
+		arr[ind1]=arr[ind2];
+		arr[ind2]=t;
 	}
 	static void sortData(String []arr) {
 		for(int i=0; i<arr.length;i++)
 			for(int j=i+1;j<arr.length;j++ ) {
-				if(arr[i]>arr[j])//compareTo() 사용
+				if(arr[i].compareTo(arr[j])>0)//compareTo() 사용
 					swap(arr,i,j);
 			}
 	}
 
 	}
 
-}
