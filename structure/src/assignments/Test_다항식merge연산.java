93e6e7c784e1198901467fa21cccb52cf8639a6c
JuneKim <junekim@localhost>
2024-03-25 22:21:35 +0900

    에러수정하기

--- structure/src/assignments/Test_다항식merge연산.java
+++ structure/src/assignments/Test_다항식merge연산.java
@@ -24,6 +24,26 @@ public class Test_다항식merge연산 {
 
 	static void merge(Polynomial[] a, int lefta, int righta, int leftb, int rightb ) {
 		//배열을 반으로 나누었을때 왼쪽배열의 양끝을 lefta, leftb라고 하고 오른쪽 양끝을 righta, rightb라고 한다.
+		Polynomial[] temp=new Polynomial[30];
+		int ix=0;//임시배열에 새로운 요소를 삽입할 때 사용하는 인덱스
+		int p=lefta, q=leftb;
+		while(p<righta && q<rightb) {
+			if(a[p].compareTo(a[q])<0) temp[ix++]=a[p++];
+			else if(a[p].compareTo(a[q])>0) temp[ix++]=a[q++];
+			else {
+				temp[ix++]=a[p++];
+				temp[ix++]=a[q++];
+			}
+		}
+		while(p>righta && q<=rightb) {
+			temp[ix++]=a[q++];
+		}
+		while(p<=rightb && q>rightb) {
+			temp[ix++]=a[p++];
+		}
+		for(int j=0; j<ix;j++) {
+			System.out.print(a[lefta+j]+" ");//lefta+j 왜죠??????????
+		}
 
 	}
 
@@ -76,23 +96,63 @@ public class Test_다항식merge연산 {
 	}
 
 	static int EvaluatePolynomial(Polynomial[] z, int i) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result=0;
+		for(int j=0; j<z.length;j++) {
+			result += z[j].coef*Math.pow(i,z[j].exp);
+		}
+		return result;
 	}
 
 	static void MultiplyPolynomial(Polynomial[] x, Polynomial[] y, Polynomial[] z) {
-		// TODO Auto-generated method stub
-		
+		int k=0;
+		for(int i=0; i< x.length;i++) {
+			for(int j=0; j<y.length;j++) {
+				z[k++]=new Polynomial(x[i].coef*y[j].coef, x[i].exp+y[j].exp);
+			}
+		}
+		//동류항 더하기 
+		for(int i=0;i<z.length;i++) {
+			for(int j=1;j<z.length;j++) {
+				if(z[i].exp==z[j].exp) {
+					z[i].coef+=z[j].coef;
+					z[j].coef=0;
+				}
+			}
+		}
+			
 	}
 
 	static void AddPolynomial(Polynomial[] x, Polynomial[] y, Polynomial[] z) {
-		if(y[i]==z[i])
-			x[]
-		
+		int i=0,j=0,k=0;
+		while(i<x.length &&j<y.length) {//길이가 같은부분 계산 
+			if(x[i].exp==y[j].exp) {
+				z[k++]=new Polynomial(x[i].coef+y[j].coef, x[i].exp);
+				i++;
+				j++;
+			}else if(x[i].exp<y[j].exp) {
+				z[k++]=new Polynomial(y[j].coef,y[j].exp);
+				j++;
+			}else {
+				z[k++]=new Polynomial(x[i].coef,x[i].exp);
+				i++;
+			}
+
+		}//엑스식이 더 길다면 추가해 줌 
+		while(i<x.length) {
+			z[k++]=new Polynomial(x[i].coef,x[i].exp);
+			i++;
+		}//와이식이 더 길때도 추가해 
+		while(j<y.length) {
+			z[k++]=new Polynomial(y[j].coef,y[j].exp);
+			j++;
+		}
 	}
 
 	static void ShowPolynomial(Polynomial[] x) {
-		// TODO Auto-generated method stub
+		for(int i=0; i<x.length;i++) {
+			System.out.print(x[i].coef+"X^"+x[i].exp+" + ");
+		}
+		System.out.println();
 		
 	}
 }
